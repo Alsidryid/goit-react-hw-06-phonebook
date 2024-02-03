d03692c39cc549e46c41d93ba3732aa5f30e1646
Aleksey S <94798542+Alsidryid@users.noreply.github.com>
2024-02-03 22:39:56 +0200

    add component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,48 +1,9 @@
-import { useSelector, useDispatch } from 'react-redux';
-import { addContact, deleteContact } from '../redux/contacts/contacts-slice';
-import { setFilter } from '../redux/filter/filter-slice';
-import ConstactsList from './ContactsList/ContactsList';
-import PhoneForm from './PhoneForm/PhoneForm';
-import Filter from './Filter/Filter';
-import style from './App.module.css';
-import { getFiltredContacts } from '../redux/contacts/contact-selectors';
-
+import ContactsAddApp from './ContactsAddApp/ContactsAddApp';
 const App = () => {
-  const contacts = useSelector(getFiltredContacts);
-
-  const dispatch = useDispatch();
-
-  const isDublicate = ({ name }) => {
-    const normalizetName = name.toLowerCase();
-    const dublicateName = contacts.find(item => {
-      const normalizetCurrentName = item.name.toLowerCase();
-      return normalizetCurrentName === normalizetName;
-    });
-    return Boolean(dublicateName);
-  };
-
-  const onAddContact = formState => {
-    if (isDublicate(formState)) {
-      return alert(`${formState.name} is already in contacts`);
-    }
-
-    dispatch(addContact(formState));
-  };
-
-  const onDeleteContact = id => {
-    dispatch(deleteContact(id));
-  };
-
-  const changeFilter = ({ target }) => dispatch(setFilter(target.value));
-
   return (
-    <div className={style.box}>
-      <h1 className={style.title}>Phonebook</h1>
-      <PhoneForm onSubmit={onAddContact} />
-      <h2 className={style.title}>Contacts</h2>
-      <Filter onChange={changeFilter} />
-      <ConstactsList items={contacts} onDeleteContact={onDeleteContact} />
-    </div>
+    <>
+      <ContactsAddApp />
+    </>
   );
 };
 
